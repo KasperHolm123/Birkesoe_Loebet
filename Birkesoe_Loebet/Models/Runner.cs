b0def47dcca9d30e2e4e18b367644ee3a2c795b5
RaceLuden <boii@outlook.dk>
2022-01-03 13:33:31 +0100

    Opprettelse af løber og distance modeler

---
+++ Birkesoe_Loebet/Models/Runner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birkesoe_Loebet.Models
+{
+    /// <summary>
+    /// Model for hver enkelte løber. 
+    /// Modelen bruges som skiste der skal indeholde data om de enkelte løbere
+    /// </summary>
+    public class Runner
+    {
+        public string Name { get; set; }
+        public string RunnerAddress { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public List<string> RunningCourses = new List<string>(); // Hver enkelte løber kan tilmælde sig én eller flere distancer
+        public int EndTime { get; set; }
+
+    }
+}
---
+++ Birkesoe_Loebet/Models/RunningCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birkesoe_Loebet.Models
+{
+    class RunningCourse
+    {
+        public int CourseDistance { get; set; }
+        public int StartTime { get; set; }
+    }
+}
